3d3a3d5af4530ed56ed3c6eaaa05f25f389c6370
Mercynenoh <mercychepngenoh74@gmail.com>
2022-04-10 00:44:44 +0300

    add quote class

---
+++ src/app/quote.ts
@@ -0,0 +1,16 @@
+export class Goal {
+    
+
+  showDescription: boolean;
+constructor(
+    public id: number,
+    public name: string,
+    public upvote:number,
+    public downvote:number,
+    public description: string,
+    public yourname: string,
+    public completeDate: Date
+    ){
+  this.showDescription=false;
+}
+}
